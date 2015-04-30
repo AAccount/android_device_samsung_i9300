6e606640f1cd93afd96c4daa18e1455907bb9c68
Daniel Tjandra <danieltjandra@gmail.com>
2015-04-29 23:43:16 -0400

    include omni gsm parts

--- full_i9300.mk
+++ full_i9300.mk
@@ -23,6 +23,10 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
 $(call inherit-product, device/samsung/i9300/i9300.mk)
